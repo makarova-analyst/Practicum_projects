bf7d7ae0e5482adced963489a03be5dc67675dfa
Елена Макарова <makarova.ev@list.ru>
2025-10-30 15:33:08 +0300

    Create 2_conversion_rate.sql

---
+++ food_delivery_analysis/sql_queries/2_conversion_rate.sql
@@ -0,0 +1,13 @@
+-- Название: Расчёт Conversion Rate (CR)
+-- Проект: Анализ сервиса доставки еды «Всё.из.кафе»
+
+SELECT 
+    ae.log_date,
+    ROUND(COUNT(DISTINCT ae.user_id) filter (WHERE ae.event ='order')/ 
+          COUNT(DISTINCT ae.user_id)::numeric, 2) AS CR
+FROM analytics_events AS ae
+JOIN cities AS c ON ae.city_id = c.city_id
+WHERE c.city_name = 'Саранск'
+AND ae.log_date BETWEEN '2021-05-01' AND '2021-06-30'
+GROUP BY ae.log_date
+ORDER BY ae.log_date ASC;
