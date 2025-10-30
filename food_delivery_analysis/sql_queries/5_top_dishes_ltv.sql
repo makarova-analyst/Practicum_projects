ebc7e2c13aadd799a7686477600ce6b47328f85b
Елена Макарова <makarova.ev@list.ru>
2025-10-30 16:56:10 +0300

    Update 5_top_dishes_ltv.sql

--- food_delivery_analysis/sql_queries/5_top_dishes_ltv.sql
+++ food_delivery_analysis/sql_queries/5_top_dishes_ltv.sql
@@ -9,34 +9,34 @@ WITH orders AS
   (SELECT events.rest_id,
           events.city_id,
           events.object_id,
-          revenue * commission AS commission_revenue
+          revenue * commission AS commission_revenue  -- Комиссия сервиса с заказа
    FROM analytics_events AS events
-   JOIN cities ON events.city_id = cities.city_id
-   WHERE revenue IS NOT NULL
+   JOIN cities ON events.city_id = cities.city_id     -- Соединяем для фильтрации по городу
+   WHERE revenue IS NOT NULL    -- Только заказы с известной выручкой
      AND log_date BETWEEN '2021-05-01' AND '2021-06-30'
      AND city_name = 'Саранск'), 
 -- Рассчитываем два ресторана с наибольшим LTV
 top_ltv_restaurants AS
   (SELECT orders.rest_id,
-          chain,
-          type,
-          ROUND(SUM(commission_revenue)::numeric, 2) AS LTV
+          chain,  -- Название сети ресторанов
+          type,   -- Тип кухни
+          ROUND(SUM(commission_revenue)::numeric, 2) AS LTV   -- Общая комиссия за период
    FROM orders
    JOIN partners ON orders.rest_id = partners.rest_id AND orders.city_id = partners.city_id 
-   GROUP BY 1, 2, 3
+   GROUP BY 1, 2, 3   -- Группировка по ресторану, сети и типу кухни 
    ORDER BY LTV DESC
-   LIMIT 2)
+   LIMIT 2)          -- Выбираем только два самых доходных ресторана
   -- Выводим пять блюд с максимальным LTV
 SELECT chain AS "Название сети",
        dishes.name AS "Название блюда",
        spicy,
        fish,
        meat,
-       ROUND(SUM(orders.commission_revenue)::numeric, 2) AS LTV
+       ROUND(SUM(orders.commission_revenue)::numeric, 2) AS LTV   -- Суммарная комиссия от блюда
 FROM orders
-JOIN top_ltv_restaurants ON orders.rest_id = top_ltv_restaurants.rest_id
-JOIN dishes ON orders.object_id = dishes.object_id
+JOIN top_ltv_restaurants ON orders.rest_id = top_ltv_restaurants.rest_id   -- Только топ-рестораны
+JOIN dishes ON orders.object_id = dishes.object_id   -- Информация о блюдах
 AND top_ltv_restaurants.rest_id = dishes.rest_id
-GROUP BY 1, 2, 3, 4, 5
+GROUP BY 1, 2, 3, 4, 5   -- Группировка по характеристикам блюда
 ORDER BY LTV DESC
-LIMIT 5;
+LIMIT 5;  -- Только 5 самых доходных блюд
