d88c46bd84382fa0dde77504347e761007b533ae
Елена Макарова <makarova.ev@list.ru>
2025-10-30 15:38:24 +0300

    Create 3_avg_check.sql

---
+++ food_delivery_analysis/sql_queries/3_avg_check.sql
@@ -0,0 +1,22 @@
+-- Название: Расчёт среднего чека
+-- Проект: Анализ сервиса доставки еды «Всё.из.кафе»
+-- Рассчитываем величину комиссии с каждого заказа, отбираем заказы по дате и городу
+WITH orders AS
+    (SELECT *,
+            revenue * commission AS commission_revenue
+     FROM analytics_events
+     JOIN cities ON analytics_events.city_id = cities.city_id
+     WHERE revenue IS NOT NULL
+         AND log_date BETWEEN '2021-05-01' AND '2021-06-30'
+         AND city_name = 'Саранск')
+
+-- Рассчитываем средний чек
+SELECT 
+    CAST(DATE_TRUNC('month', log_date) AS date)
+ AS "Месяц",
+    COUNT(DISTINCT order_id) AS "Количество заказов",
+    ROUND(SUM(commission_revenue)::numeric, 2) AS "Сумма комиссии",
+    ROUND((SUM(commission_revenue) / COUNT(DISTINCT order_id))::numeric, 2) AS "Средний чек"
+FROM orders
+GROUP BY CAST(DATE_TRUNC('month', log_date) AS date)
+ORDER BY "Месяц" ASC
